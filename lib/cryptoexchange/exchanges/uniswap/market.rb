aef7334ffbfec2d9452d09df5a19eeee5c9a0e98
Matthew Hui <mh@berkeley.edu>
2020-12-17 13:05:36 -0800

    Update Uniswap API URL

--- lib/cryptoexchange/exchanges/uniswap/market.rb
+++ lib/cryptoexchange/exchanges/uniswap/market.rb
@@ -2,7 +2,7 @@ module Cryptoexchange::Exchanges
   module Uniswap
     class Market < Cryptoexchange::Models::Market
       NAME = 'uniswap'
-      API_URL = "https://api.blocklytics.org/pools/v1"
+      API_URL = "https://data-api.defipulse.com/api/v1/blocklytics/pools/v1"
 
       def self.api_key
         authentication = Cryptoexchange::Exchanges::Uniswap::Authentication.new(
